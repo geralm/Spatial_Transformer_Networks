e523e354b480239bb65e746992b66df0a3cdb284
geralm <estebanlm@estudiantec.cr>
2024-04-08 12:50:06 -0600

    Some basics installation

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.10.12
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+RUN pip install --upgrade pip
+RUN pip3 install -r requirements.txt
 RUN mkdir -p /home/app
 COPY . /home/app
 WORKDIR /home/app
--- src/app.py
+++ src/app.py
@@ -1,3 +1,6 @@
-#print "module name:", __name__
+import numpy as np
+import torch
 if __name__ == "__main__":
-    print("Hello World!")
+    print("Versión de NumPy:", np.__version__)
+    print("Versión de PyTorch:", torch.__version__)
+
